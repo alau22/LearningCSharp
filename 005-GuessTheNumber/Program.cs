183689ffdebbd4bd8e132b5824827e97887b8d6a
Audrey Nault <audreynault@hotmail.com>
2019-12-27 21:11:47 -0500

    trouver le nombre

---
+++ 005-GuessTheNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+  class Program
+  {
+    const int MaximumTry = 7;
+
+    static int Main()
+    {
+      var random = new Random();
+      int number = random.Next(1, 101);
+
+      int x = 1;
+      while (x <= MaximumTry)
+      {
+        Console.WriteLine("Choisissez un nombre de 1 à 100");
+        int guess = int.Parse(Console.ReadLine());
+        if (guess < number)
+        {
+          Console.WriteLine("Trop petit");
+        }
+        else if (guess > number)
+        {
+          Console.WriteLine("Trop grand");
+        }
+        else
+        {
+          Console.WriteLine("Bravo!");
+          return 0;
+        }
+        x++;
+      }
+      Console.WriteLine("Perdant :'(");
+      return 1;
+    }
+  }
+
+}
